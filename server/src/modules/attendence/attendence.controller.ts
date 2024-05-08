c8b05d45fb003048a1d9c38596d3bfc46ad31682
Nishant <srnikhil3721@gmai.com>
2024-05-09 01:09:18 +0530

    change

---
+++ server/src/modules/attendence/attendence.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { AttendenceService } from './attendence.service';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+
+@Controller('attendence')
+export class AttendenceController {
+  constructor(private readonly attendenceService: AttendenceService) {}
+
+  @Post('add')
+  @UseGuards(JwtAuthGuard)
+  async addAttendence(@Req() req: any) {
+    return this.attendenceService.addAttendence(req.body);
+  }
+
+  @Get('get-all')
+  @UseGuards(JwtAuthGuard)
+  async getAttendences(@Req() req: any) {
+    return this.attendenceService.getAttendences(req.user);
+  }
+}
